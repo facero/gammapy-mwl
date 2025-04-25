44985c23bcc85bffaac8fc36a5b2e5b7c6f6b2f5
Régis Terrier <rterrier@apc.in2p3.fr>
2025-04-25 13:56:00 +0200

    Modified __init__ for versioning
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- gammapy_mwl/__init__.py
+++ gammapy_mwl/__init__.py
@@ -1,4 +1,12 @@
 
 from .version import version as __version__
+from importlib.metadata import version as _version, PackageNotFoundError
+
+try:
+    __version__ = _version(__name__)
+except PackageNotFoundError:
+    pass
+del version, PackageNotFoundError
+
 
 __all__ = []
